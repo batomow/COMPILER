cf4c7f5787d9a5a87fb334acdff9802cb03e6570
iangranados <ian.live1@gmail.com>
2019-11-29 10:11:44 -0600

    VM 30%

--- vm/main.gd
+++ vm/main.gd
@@ -3,87 +3,165 @@ var quads:Array = []
 # warning-ignore:unused_class_variable
 var globals_counter:int = 0
 var globals := []
-var locals_counter:int = 0
-var locals := []
+var localsArray := []
+var eraArray := []
 var constants_counter:int = 0
 var constants := []
 var flag = true
 var stackPointer= []
-var currentPointer:int
+const DEBUGGING = 0
 
 var return_buffer:int
 
 func _ready():
+	# Initialize pointer stack, used to know where we are reading
 	stackPointer.append(0)
-	currentPointer = 0
-	for n in range(100):
-		globals.append(0)
-		locals.append(0)
-		constants.append(0)
+	
+	# Initialize localsArray, the structure used to hold the memory arrays with local memory
+	var mainLocal = []
+	mainLocal.resize(100)
+	localsArray.append(mainLocal)
+	
+	# Initialize Global memory
+	globals.resize(100)
+
 	_load() 
 
 func _load():
+	# Load object file
 	var file = File.new(); 
 	file.open("res://main.fso", file.READ);
+	
+	# Parse object file's json code
 	var text:String = file.get_as_text()
 	var input = parse_json(text)
+	
+	# Get quadruple list from json
 	quads = input.quads
+	
+	if(DEBUGGING):
+		for i in range(quads.size() - 1):
+			print(i, ": ", quads[i])
+	
+	# Set constants in memory
 	var constantAux = input.globals
+	constantAux.pop_back()
+	constants.resize(constantAux.size())
 	for registro in constantAux:
 		var i = int(registro.memdir - 1000)
-		if i >= 0:
-			constants[i] = registro.valor
+		constants[i] = registro.valor
+	
+	# Close object file
 	file.close()
 
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	if flag: 
-		var quad = quads[stackPointer[currentPointer]]
-		if quad.opcode == 0:
+	if (flag && !stackPointer.empty()):
+		# Get current quad 
+		var quad = quads[stackPointer.back()]
+		if quad.opcode == 0: # SUM
 			_op_sum(quad)
-		elif quad.opcode == 10:
+		elif quad.opcode == 1: # SUBSTRACT
+			_op_res(quad)
+		elif quad.opcode == 4: # MULT
+			_op_mult(quad)
+		elif quad.opcode == 6: # GOTO
+			_op_goto(quad)
+		elif quad.opcode == 7: # GOTOF
+			_op_gotof(quad)
+		elif quad.opcode == 9: # GOSUB
+			_op_gosub(quad)
+		elif quad.opcode == 10: # ASSIGN
 			_op_assign(quad)
-		elif quad.opcode == 22:
+		elif quad.opcode == 11: # PRINT
+			_op_print(quad)
+		elif quad.opcode == 14: # GREATER THAN
+			_op_gt(quad)
+		elif quad.opcode == 19: # EQUALS
+			_op_eeq(quad)
+		elif quad.opcode == 21: # END PROCEDURE
+			_op_endproc(quad)
+		elif quad.opcode == 22: # END PROGRAM 
 			flag = false
-			print(locals, "\n", globals, "\n", constants)
-		stackPointer[currentPointer] += 1
+			if DEBUGGING:
+				print("Globals: ", globals, "\nConstants: ", constants)
+		elif quad.opcode == 25: # ERA
+			_op_era(quad)
+		elif quad.opcode == 26: # PARAM
+			_op_param(quad)
+		elif quad.opcode == 27: # RETURN
+			_op_return(quad)
+		
+		if stackPointer.empty():
+			flag = false
+		else:
+			stackPointer[-1] += 1
 
-func _get_mem_val(addr):
+func _get_mem_val(addr, local = -1):
+	
+	if(addr == 66 ): #return buffer
+		return return_buffer
 	if (addr < 2000): #is a constant
+		if DEBUGGING:
+			print("_get_mem_val|Constants: ", constants)
 		var i = addr - 1000
 		return constants[i]
 	if(addr >= 2000 && addr < 3000): #is global
+		if DEBUGGING:
+			print("_get_mem_val|Globals: ", globals)
 		var i = addr - 2000
-		return constants[i]
+		return globals[i]
 	if(addr >= 3000): # is local
+		if DEBUGGING:
+			print("_get_mem_val|Locals: ", localsArray)
 		var i = addr - 3000
-		return constants[i]
+		return localsArray[local][i]
 
-func _set_mem_addr(addr, val):
-	if (addr < 2000): #is a constant
-		if constants[addr-100] == 0:
-			constants_counter += 1
-		constants[addr-1000] = val
-	elif(addr >= 2000 && addr < 3000): #is global
-		if globals[addr-2000] == 0:
-			constants_counter += 1
-		globals[addr-2000] = val
-	elif(addr >= 3000): # is local
-		if locals[addr-3000] == 0:
-			locals_counter += 1
-		locals[addr-3000] = val
+func _set_mem_addr(addr, val, isParam = 0):
+	if isParam:
+		eraArray[-1][addr-3000] = val
+		if DEBUGGING:
+			print("_set_mem_addr|Eras: ", eraArray)
+	else:
+		if(addr == 66 ): #return buffer
+			return_buffer = val
+		elif (addr < 2000): #is a constant
+			if DEBUGGING:
+				print("_set_mem_addr|Constants: ", constants)
+			if constants[addr-100] == 0:
+				constants_counter += 1
+			constants[addr-1000] = val
+		elif(addr >= 2000 && addr < 3000): #is global
+			if DEBUGGING:
+				print("_set_mem_addr|Globals: ", globals)
+			if globals[addr-2000] == 0:
+				globals_counter += 1
+			globals[addr-2000] = val
+		elif(addr >= 3000): # is local
+			if DEBUGGING:
+				print("_set_mem_addr|Locals: ", localsArray)
+			localsArray[-1][addr-3000] = val
+		
 
 func _op_sum(quad):
+	if DEBUGGING:
+		print("SUM: ", quad)
 	var left = _get_mem_val(quad.left)
 	var right = _get_mem_val(quad.right)
 	_set_mem_addr(quad.result, left + right)
-#
+	if DEBUGGING:
+		print(left, " + ", right, " = ", _get_mem_val(quad.result))
+
 # warning-ignore:unused_argument
 func _op_res(quad):
+	if DEBUGGING:
+		print("RES: ", quad)
 	var left = _get_mem_val(quad.left)
 	var right = _get_mem_val(quad.right)
 	_set_mem_addr(quad.result, left - right)
+	if DEBUGGING:
+		print(left, " - ", right, " = ", _get_mem_val(quad.result))
 
 # warning-ignore:unused_argument
 func _op_root(quad):
@@ -95,7 +173,13 @@ func _op_div(quad):
 
 # warning-ignore:unused_argument
 func _op_mult(quad):
-	pass
+	if DEBUGGING:
+		print("MULT: ", quad)
+	var left = _get_mem_val(quad.left)
+	var right = _get_mem_val(quad.right)
+	_set_mem_addr(quad.result, left * right)
+	if DEBUGGING:
+		print(left, " * ", right, " = ", _get_mem_val(quad.result))
 
 # warning-ignore:unused_argument
 func _op_pow(quad):
@@ -103,11 +187,23 @@ func _op_pow(quad):
 
 # warning-ignore:unused_argument
 func _op_goto(quad):
-	pass
+	if DEBUGGING:
+		print("GOTO: ", quad)
+	var jump = quad.result
+	stackPointer[-1] = jump - 1
+	if DEBUGGING:
+		print("Jumped to ", stackPointer[-1])
 
 # warning-ignore:unused_argument
 func _op_gotof(quad):
-	pass
+	if DEBUGGING:
+		print("GOTOF: ", quad)
+	var left = _get_mem_val(quad.left)
+	if(!left):
+		var jump = quad.result
+		stackPointer[-1] = jump - 1
+	if DEBUGGING:	
+		print("Jumped to ", stackPointer[-1])
 
 # warning-ignore:unused_argument
 func _op_gotov(quad):
@@ -115,17 +211,29 @@ func _op_gotov(quad):
 
 # warning-ignore:unused_argument
 func _op_gosub(quad):
-	pass
+	if DEBUGGING:
+		print("GOSUB: ", quad)
+	localsArray.append(eraArray.back())
+	eraArray.pop_back()
+	stackPointer.append(quad.result - 1)
+	if DEBUGGING:
+		print("Jumped to ", stackPointer[-1], " to start procedure")
 
 func _op_assign(quad):
+	if DEBUGGING:
+		print("ASSIGN: ", quad)
 	var left = _get_mem_val(quad.left)
 	var res = quad.result
 	
 	_set_mem_addr(res, left)
+	if DEBUGGING:
+		print(res, " = ", left, " (check: ", _get_mem_val(res), ")")
 
 # warning-ignore:unused_argument
 func _op_print(quad):
-	pass
+	if DEBUGGING:
+		print("PRINT: ", quad)
+	print(">> ", _get_mem_val(quad.result));
 
 # warning-ignore:unused_argument
 func _op_read(quad):
@@ -137,7 +245,14 @@ func _op_lt(quad):
 
 # warning-ignore:unused_argument
 func _op_gt(quad):
-	pass
+	if DEBUGGING:
+		print("GT: ", quad)
+	var left = _get_mem_val(quad.left)
+	var right = _get_mem_val(quad.right)
+	_set_mem_addr(quad.result, left > right)
+	
+	if DEBUGGING:
+		print(left, " > ", right, " = ", _get_mem_val(quad.result))
 
 # warning-ignore:unused_argument
 func _op_lte(quad):
@@ -157,7 +272,14 @@ func _op_or(quad):
 
 # warning-ignore:unused_argument
 func _op_eeq(quad):
-	pass
+	if DEBUGGING:
+		print("EQUALS: ", quad)
+	var left = _get_mem_val(quad.left)
+	var right = _get_mem_val(quad.right)
+	_set_mem_addr(quad.result, left == right)
+	
+	if DEBUGGING:
+		print(left, " == ", right, " = ", _get_mem_val(quad.result))
 
 # warning-ignore:unused_argument
 func _op_neq(quad):
@@ -165,11 +287,18 @@ func _op_neq(quad):
 
 # warning-ignore:unused_argument
 func _op_endproc(quad):
-	#free mem
-	pass
+	if DEBUGGING:
+		print("ENDPROC: ", quad)
+	return_buffer = -1
+	stackPointer.pop_back()
+	localsArray.pop_back()
+	if DEBUGGING:
+		print("Control now to: ", stackPointer[-1], ", Returned: ", return_buffer)
 
 # warning-ignore:unused_argument
 func _op_endprog(quad):
+	if DEBUGGING:
+		print("ENDPROG: ", quad)
 	flag = false
 
 # warning-ignore:unused_argument
@@ -182,13 +311,35 @@ func _op_forcheck(quad):
 
 # warning-ignore:unused_argument
 func _op_era(quad):
-	pass
+	if DEBUGGING:
+		print("ERA: ", quad)
+	var newLocal = []
+	newLocal.resize(quad.result)
+	eraArray.append(newLocal)
+	if DEBUGGING:
+		print("Added new memory chunk of size ", localsArray[-1].size(), "to ERA array")
 
 # warning-ignore:unused_argument
 func _op_param(quad):
-	pass
+	if DEBUGGING:
+		print("PARAM: ", quad)
+	var left = _get_mem_val(quad.left)
+	if DEBUGGING:
+		print (quad.left,  " = ", left)
+	_set_mem_addr(quad.result, left, 1)
+	if DEBUGGING:
+		print("Assigned parameter in ", quad.result, " as ", _get_mem_val(quad.result))
 
 # warning-ignore:unused_argument
 func _op_return(quad):
+	if DEBUGGING:
+		print("RETURN: ", quad)
 	return_buffer = _get_mem_val(quad.result)
+	stackPointer.pop_back()
+	localsArray.pop_back()
+	if DEBUGGING:
+		if stackPointer.empty() :
+			print("Program finished");
+		else:
+			print("Control now to: ", stackPointer[-1], ", Returned: ", return_buffer)
 	
