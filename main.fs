635058f403f637c542f4b81a92d8774d6c370802
batomow <carlosa_miranda@outlook.com>
2019-12-01 19:05:16 -0600

    Quick Test

--- main.fs
+++ main.fs
@@ -15,6 +15,9 @@ order enter:int(){
     vector position = {4.5, 7.7}
     vision(position.x)
     vision(position.y)
+    scan(size)
+    var result2:int = factorial(size)
+    vision(result2)
 	return 0
 }
 
