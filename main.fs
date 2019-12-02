b998971d5496eef97bd4542be52883c70ce3d4dd
batomow <carlosa_miranda@outlook.com>
2019-12-02 02:15:28 -0600

    Second Test

--- main.fs
+++ main.fs
@@ -7,11 +7,13 @@
 }*/
 
 order enter:int(){
-    element caja = {1, 0, 0, 100, 100, 10, 10}
-
-    vision(caja.posx)
+    element caja = {1, 0, 0, 100, 100, 1, 1}
+    element circulo = {1, 1, 7, 200, 100, 1, 1}
 
     vector algo = (4.4, 7.7)
+    vision(algo.x)
+    vision(algo.y)
+
     return 0
 }
 
